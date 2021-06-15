c8f4af1d5c2def3b7aa336ac0338fc755e476f35
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-15 14:32:33 +0200

    heure

--- pages/index.vue
+++ pages/index.vue
@@ -24,18 +24,13 @@ export default {
         
         return dateFormat 
       },
-    
-      
     },
-    
     mounted() {
-        const self = this /* creer une contanste self pour se referer a la variable du store */
-        this.test = setInterval(function() { /* this.test refére a window.test qui est une variable temporaire */
+        
+        this.test = setInterval(() => { /* this.test refére a window.test qui est une variable temporaire */
           let timeDate = new Date()
-          self.interval = " Il est " + timeDate.getHours() + "h" + timeDate.getMinutes() + "m" + timeDate.getSeconds() 
-              
-         }, 1000);
-      
+          this.interval = " Il est " + timeDate.getHours() + "h" + timeDate.getMinutes() + "m" + timeDate.getSeconds() 
+        }, 1000);
     }
 }
 </script>
