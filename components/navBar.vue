adbda6d8c4e0721dd64fe5ff870467cc87851244
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-16 14:46:41 +0200

    hover navlink

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,14 +1,25 @@
 <template>
 
     <header class="header">
-        <div class="header__logo">
-            <p class="g">g</p> <p>web</p> <p class="G">G</p> 
+        <div class="header__logo wrap">
+	        <div class="cube">
+		        <div class="front">g</div>
+		        <div class="back">dev</div>
+		        <div class="top"></div>
+		        <div class="bottom"></div>
+		        <div class="left">G</div>
+		        <div class="right">web</div>
+	        </div>
         </div>
+        <!-- <div class="header__logo">
+            <div class="anim--g">g</div> <div class="header__logo--web">web</div> <div class="anim--G">G</div> 
+        </div> -->
         <nav class="header__navBar">
             <NuxtLink :to="{ name: 'index'}" class="header__navBar--font">Accueil</NuxtLink> 
             <NuxtLink :to="{ name: 'projets'}" class="header__navBar--font">projets</NuxtLink> 
             <NuxtLink :to="{ name: 'cv'}" class="header__navBar--font">CV</NuxtLink> 
         </nav>
+        
     </header>
     
 </template>
