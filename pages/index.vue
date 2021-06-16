4d062b8c9fb382cb59d98f4f29c0cc46cad699c9
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-16 17:10:55 +0200

    class dans h1

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="container head__page">
+  <div class="container head__page ">
     <div class="block__presentation">
       <h1 class="block__moi">
           Bonjour,<br> 
           je suis Ghislain,<br>
           Développeur web
       </h1>
-      <div class="block__valeur">
-      <h1 class="">
+      <div >
+      <h1 class="block__valeur">
           Respect et Humilité, <br> 
           Ouvert et Communicatif, <br>
           Responsable et Autonome, <br>
-
       </h1>
     </div>
-    </div>
+  </div>
 
   </div>
 </template>
