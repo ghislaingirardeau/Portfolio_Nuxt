ed85f8adcc3d5ffb79a7a9e9fde015d33517af83
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-17 15:18:06 +0200

    test1 sur accueil

--- components/navBar.vue
+++ components/navBar.vue
@@ -18,7 +18,7 @@
             <NuxtLink :to="{ name: 'index'}" class="header__navBar--font">Accueil</NuxtLink> 
             <NuxtLink :to="{ name: 'projets'}" class="header__navBar--font">projets</NuxtLink> 
             <NuxtLink :to="{ name: 'cv'}" class="header__navBar--font">CV</NuxtLink> 
-            <NuxtLink :to="{ name: 'test'}" class="header__navBar--font">test</NuxtLink> 
+            <!-- <NuxtLink :to="{ name: 'test'}" class="header__navBar--font">test</NuxtLink>  -->
         </nav>
         
     </header>
