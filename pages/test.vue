cb9a66eb84fdfdb70c4101e6050ed0c97627f62a
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-17 10:34:04 +0200

    test accueil en cube

---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrap_index">
+	        <div class="cube_index">
+		        <div class="back_index"> 
+                    <h1>Bonjour,<br> 
+                        je suis Ghislain,<br>
+                        Développeur web
+                        </h1> 
+                    </div>
+		        <div class="left_index"> 
+                    <h1 class="left_index--rotation">
+                        Respect et Humilité, <br> 
+                        Ouvert et Communicatif, <br>
+                        Responsable et Autonome, <br>
+                        </h1> 
+                    </div>
+	        </div>
+        </div>
+</template>
