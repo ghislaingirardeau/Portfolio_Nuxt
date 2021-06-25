636c6fafd6aaaaeeb4f70b8ce7309cafca7a16d8
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-25 14:28:42 +0200

    add image

--- store/projects.js
+++ store/projects.js
@@ -12,41 +12,41 @@ export default {
             id: '2',
             name: "ohmyfood",
             summary: "Mise en œuvre une structure de navigation web",
-            description: "Mettre en œuvre des effets CSS graphiques avancés en assurant une cohérence graphique ",
-            tech: "HTML, CSS, SASS, GitHub",
+            description: "Mettre en œuvre des effets CSS graphiques avancés en assurant une cohérence graphique. Mise en place d'animation Keyframes et transitions pour rendre enrichir l'expérience utilisateur",
+            tech: "HTML, CSS, SASS, GitHub, méthode BEM pour un code clair",
             imageURL: ["ohmyfood.png", "ohmyfood_menu.png", "ohmyfood_menu1.png", "ohmyfood_loader.png"]
         },
         {
             id: '3',
             name: "La chouette agence",
             summary: "Utiliser les techniques d'amélioration et de référencement d'un site web",
-            description: "Sur la base d'un site web existant, Assurer son accessibilité, Optimiser son référencement, sa taille et son temps de chargement",
-            tech: "Aria, SEO, google analitycs, GitHub",
-            imageURL: ["reservia.png", "ohmyfood.png", "orinoco.png"]
+            description: "Sur la base d'un site web existant, améliorer son référencement par un retravail des mots clés, rendre le site responsive et assurer son accessibilité. Optimiser son référencement, sa taille et son temps de chargement",
+            tech: "Aria, SEO, google analytics, google search console, wave evaluation, headingMap  GitHub",
+            imageURL: ["chouette_agence_loading.png", "chouette_agence_titre.png", "chouette_agence_responsive.png"]
         },
         {
             id: '4',
             name: "Orinoco",
             summary: "Création d'un MVP pour un site e-commerce",
-            description: "Interagir avec une API avec JavaScript, Valider des données issues de sources externes, Créer un plan de test pour une application, Gérer des événements JavaScript",
-            tech: "Javascript, bootstrap, GitHub",
-            imageURL: ["reservia.png", "ohmyfood.png", "orinoco.png"]
+            description: "Interagir avec une API avec JavaScript. Valider des données issues de sources externes avant leur envoie vers API. Créer un plan de test pour une application et gérer des événements JavaScript",
+            tech: "Javascript, bootstrap, Regex, GitHub",
+            imageURL: ["orinoco_accueil.PNG", "orinoco_produit.PNG", "orinoco_basket.PNG", "orinoco_forms.PNG", "orinoco_confirm.PNG"]
         },
         {
             id: '5',
             name: "So Peckoko",
             summary: "Construction d'une API Rest",
-            description: "Mettre en œuvre des opérations CRUD de manière sécurisée, Stocker des données de manière sécurisée, Implémenter un modèle logique de données conformément à la réglementation OWASP",
-            tech: "Node js, Express, MongoDb, GitHub",
+            description: "Mettre en œuvre des opérations CRUD de manière sécurisée. Construire l'API en stockant des données de manière sécurisée, Implémenter un modèle logique de données conformément à la réglementation OWASP",
+            tech: "Node js, Express, bcrypt, multer, MongoDb, GitHub",
             imageURL: ["reservia.png", "ohmyfood.png", "orinoco.png"]
         },
         {
             id: '6',
             name: "Groupomania",
             summary: "Création d'un réseau social interne à une entreprise",
-            description: "Personnaliser le contenu envoyé à un client web, Gérer un stockage de données à l'aide de SQL, Implémenter un stockage de données sécurisé en utilisant SQL, Authentifier un utilisateur et maintenir sa session",
-            tech: "Mysql, Node js, Express, bootstrap, Vue js, GitHub",
-            imageURL: ["groupomania.png"]
+            description: "Personnaliser le contenu envoyé à un client web, Gérer un stockage de données à l'aide de SQL, Implémenter un stockage de données sécurisé en utilisant SQL, Authentifier un utilisateur et maintenir sa session, Définir des rôles et des privilèges suivant l'utilisateur",
+            tech: "Mysql, Node js, Express, bcrypt, multer, bootstrap, Vue js, GitHub",
+            imageURL: ["groupomania_log.png", "groupomania_signup.png", "groupomania_home.png", "groupomania_delete.png", "groupomania_forum.png", "groupomania_update.png", "groupomania_empty.png"]
         }
     ]
 }
