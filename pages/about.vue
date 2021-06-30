7db91047e18471b3417a8c182f61225b8e5816b7
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-30 11:50:00 +0200

    store content about

--- pages/about.vue
+++ pages/about.vue
@@ -14,40 +14,21 @@
                     <h2>Qui suis-je...</h2>
 
                     <h3>...Personnellement</h3>
-                        <p>Après 10 d'expériences dans la distribution d'articles de sport, j'ai décidé de réaliser un vieux
-                            rêve et de partir faire un tour du monde pendant plus de 2 ans. <br>
-                            Une aventure riche personnellement qui m'a ouvert de nouvelles valeurs :
-                            autonomie, ouverture d'esprit, aventurier, curieux, respect. <br>
-                            Passionée de sports, investi dans le développement web, j'ai aujourd'hui le souhait de mettre à disposition mes compétences dans des projets
-                            enrichissant tant humainement que professionnellement.
+                        <p>{{personal}}
 
                         </p>
                     <h3>...Professionnellement</h3>
-                        <p> Auto-entrepreneur et freelance en developpement web :
-                            Réalisation de ce e-portfolio via Vue et Nuxt Js, 2 framework js que j'apprécie particulièrement !
-                        </p>
-                        <p> OPEN CLASSROOM : Formation 6 mois développeur Web.
-                            Partisans des méthodes AGILE, tous mes projets sont issus du monde professionnel et ont éte préenté à distance en visio conférence.
-                            J'ai organisé mon travail en tout autonomie dans le respect des délais impartis.
-                        </p>
+                        <p> {{experience.id1}}</p>
+                        <p> {{experience.id2}}</p>
                 </article>
                 <article>
                     <h2>Mes souhaits d'engagements</h2>
                     
-                    <p>Parce que les nouvelles technologies ont aussi un impact sur l'environnement... J'ai le souhait de 
-                        m'engager sur des projets ou des entreprises partageant l'idée de réduire notre consommation en énergie.
-                    </p>
-                    <p>Parce que la protection des données est un enjeu très important dans un monde oú tout va plus vite,
-                        oú tout devient dématérialisé... la confiance, le respect, la confidentialité sont des valeurs indispensables.  
-                    </p>
-                    <p>Parce que les nouvelles technologies nous facilitent la vie... Il ne faut pas oublier qu'il y a des individus
-                        derrières celles-ci : utilisateurs, clients, employés, développeurs, entrepreneur... Faire que la relation entre les 
-                        différents acteurs soient la plus saine, équitable, juste et constructive est une notion indispensables pour moi. 
-                    </p>
-                    <p>Parce que le métier de freelance nécessite une relation de confiance et de l'autonomie... Je fais des points réguliers 
-                        sur l'avancée du projet, planifie mes objectifs pour tenir les délais, discute des feedbacks constructifs pour 
-                        faire aboutir le projet.
-                    </p>
+                    <p>{{willingness.id1}}</p>
+                    <p>{{willingness.id2}}</p>
+                    <p>{{willingness.id3}}</p>
+                    <p>{{willingness.id4}}</p>
+
                 </article>
             </div>
         </transition>
@@ -56,11 +37,15 @@
 </template>
 
 <script>
+import about from "@/store/about"
 export default {
 
     data() {
         return {
-            downloadUrl: ''
+            downloadUrl: '',
+            personal: about.personaly.text,
+            experience: about.professionaly,
+            willingness: about.willingness
         }
     },
     /* methods: {
---
+++ store/about.js
@@ -0,0 +1,15 @@
+export default {
+    personaly : {
+        text: "Après 10 d'expériences dans la distribution d'articles de sport, j'ai décidé de réaliser un vieux rêve et de partir faire un tour du monde pendant plus de 2 ans. Une aventure riche personnellement qui m'a ouvert de nouvelles valeurs : autonomie, ouverture d'esprit, aventurier, curieux, respect. Passionée de sports, investi dans le développement web, j'ai aujourd'hui le souhait de mettre à disposition mes compétences dans des projets enrichissant tant humainement que professionnellement."
+    },
+    professionaly : {
+        id1: "Auto-entrepreneur et freelance en developpement web : Réalisation de ce e-portfolio via Vue et Nuxt Js, 2 framework js que j'apprécie particulièrement !",
+        id2: "OPEN CLASSROOM : Formation 6 mois développeur Web. Partisans des méthodes AGILE, tous mes projets sont issus du monde professionnel et ont éte préenté à distance en visio conférence. J'ai organisé mon travail en tout autonomie dans le respect des délais impartis."
+    },
+    willingness: {
+        id1: "Parce que les nouvelles technologies ont aussi un impact sur l'environnement... J'ai le souhait de m'engager sur des projets ou des entreprises partageant l'idée de réduire notre consommation en énergie.",
+        id2: "Parce que la protection des données est un enjeu très important dans un monde oú tout va plus vite, oú tout devient dématérialisé... la confiance, le respect, la confidentialité sont des valeurs indispensables.",
+        id3: "Parce que les nouvelles technologies nous facilitent la vie... Il ne faut pas oublier qu'il y a des individus derrières celles-ci : utilisateurs, clients, employés, développeurs, entrepreneur... Faire que la relation entre les  différents acteurs soient la plus saine, équitable, juste et constructive est une notion indispensables pour moi. ",
+        id4: "Parce que le métier de freelance nécessite une relation de confiance et de l'autonomie... Je fais des points réguliers sur l'avancée du projet, planifie mes objectifs pour tenir les délais, discute des feedbacks constructifs pour faire aboutir le projet."
+    }
+}
