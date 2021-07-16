134a4d58bcf794f5ffb611345ca88032af2aa4f9
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-16 10:33:34 +0200

    translate content project

--- locales/en.js
+++ locales/en.js
@@ -34,11 +34,11 @@ export default{
         professionaly : {
             h3: "...Profesionaly",
             id1: "Freelancer, I'm self-employed since june 2021 as web developer : I realized this e-portfolio via Vue et Nuxt Js, 2 js framework that I particulary like to work with !",
-            id2: "OPEN CLASSROOM : 6 months Web developer graduation. AGILE methods practician, all my training projects come from profesionals missions. This projects was approved and asset by a tutor, a professor and a jury commity composed by profesional developer. I used to organize my work in autonomous regarding the project's deadline defined with the customer."
+            id2: "OPEN CLASSROOM : 6 months Web developer graduation. AGILE methods practician, all my training come from real-work projects. This projects was approved and asset by a tutor, a professor and a jury commity composed by profesional developer. I used to organize my work in autonomous regarding the project's deadline defined with the customer."
         },
         willingness: {
             id1: "Because digitalisation get also an impact on the environment... I would like to work on project sharing the idea to consume less and used recycle energy.",
-            id2: "Because in a fastest and dematerialized world, the personal data protection is an important stake... Be trustable and respectful are the main values for a confident relationship.",
+            id2: "Because in a fastest and dematerialized world, the personal data protection is an important stake... Be trustable and respectful are the main values for a confident relationship in order to warranty customer data security.",
             id3: "Because technology also ease our casual life through none physcial connection... I like to keep in mind, there's always humain behind. Made clear, fair, constructive trade is the key of my work. ",
             id4: "Because be freelancer mean be autonomous, responsible, and organisate... Plan my work, have a schedules, put deadlines report and present regulary the forward of the project are my work's commitment."
         },
@@ -52,11 +52,11 @@ export default{
         },
         stepOne: {
             h3: "Step 1",
-            description: "<ul> <li> Projects presentation and willingness</li> <li>Define your need, strategy and ressource</li><li> What and how can I help you in your digitalisation</li><li>First feddback on your projects sharing about my knowledge/skills fitting or not your project</li> </ul>"
+            description: "<ul> <li> Projects presentation and willingness</li> <li>Define your need, strategy and ressource</li><li> What and how can I help you in your digitalisation process</li><li>First feddback on your projects sharing about my knowledge/skills fitting or not your project</li> </ul>"
         },
         stepTwo: {
             h3: "Step 2",
-            description: "<ul> <li>Get the specific details and need for your project</li> <li>Possibilities, solutions and recommendations regarding your strategy</li><li> Offer, delay and clear detail about the expected outcome for both of us</li><li>Questions and answers if it's still not clear</li> </ul>"
+            description: "<ul> <li>Get the specific details (need, scope and features) for your new application</li> <li>Possibilities, solutions and recommendations regarding your strategy</li><li> Offer, deadlines and clear detail about the expected outcome for both of us</li><li>Questions and answers if it's still not clear</li> </ul>"
         },
         stepThree: {
             h3: "Step 3",
@@ -68,49 +68,49 @@ export default{
         {
             id: '1',
             name: "Reservia",
-            summary: "Intégration d'une maquette en site web",
-            description: "Implémenter une interface responsive, Utiliser un système de gestion de versions pour le suivi du projet",
+            summary: "Turn Mock-ups into a Web Page",
+            description: "Get started with HTML & CSS by implementing a travel booking platform's website from scratch.",
             tech: "HTML, CSS, GitHub",
             imageURL: ["reservia.png"]
         },
         {
             id: '2',
             name: "Ohmyfood",
-            summary: "Mise en œuvre une structure de navigation web",
-            description: "Mettre en œuvre des effets CSS graphiques avancés en assurant une cohérence graphique. Mise en place d'animation Keyframes et transitions pour rendre enrichir l'expérience utilisateur",
-            tech: "HTML, CSS, SASS, GitHub, méthode BEM pour un code clair",
+            summary: "Integrate a mobile website with animations in CSS",
+            description: "Implement the mobile version of this foodtech website with CSS animations.",
+            tech: "HTML, CSS, SASS, GitHub, BEM methods for a clear code",
             imageURL: ["ohmyfood.png", "ohmyfood_menu.png", "ohmyfood_menu1.png"],
             loader: 'true'
         },
         {
             id: '3',
             name: "La chouette agence",
-            summary: "Utiliser les techniques d'amélioration et de référencement d'un site web",
-            description: "Sur la base d'un site web existant, améliorer son référencement par un retravail des mots clés, rendre le site responsive et assurer son accessibilité. Optimiser son référencement, sa taille et son temps de chargement",
+            summary: "Optimize an existing Website",
+            description: "Research and implement current best practices and standards in web development, including SEO, size and speed, and accessibility.",
             tech: "Aria, SEO, google analytics, google search console, wave evaluation, headingMap  GitHub",
             imageURL: ["chouette_agence_loading.png", "chouette_agence_titre.png", "chouette_agence_responsive.png"]
         },
         {
             id: '4',
             name: "Orinoco",
-            summary: "Création d'un 1er projet pour un site e-commerce",
-            description: "Interagir avec une API avec JavaScript. Valider des données issues de sources externes avant leur envoie vers API. Créer un plan de test pour une application et gérer des événements JavaScript",
+            summary: "Build an eCommerce Website",
+            description: "Use the JavaScript programming language to interact with users and external services. Apply a test-driven approach to web development.",
             tech: "Javascript, bootstrap, Regex, GitHub",
             imageURL: ["orinoco_accueil.PNG", "orinoco_produit.PNG", "orinoco_basket.PNG", "orinoco_forms.PNG", "orinoco_confirm.PNG"]
         },
         {
             id: '5',
             name: "So Peckoko",
-            summary: "Construction d'une interface de programmation applicative (API Rest)",
-            description: "Mettre en œuvre des opérations CRUD de manière sécurisée. Construire l'API en stockant des données (data personnel, image, texte...) de manière sécurisée, Implémenter un modèle logique de données conformément à la réglementation OWASP",
+            summary: "Build a Secure API for a review app",
+            description: "Hot Takes is a new app for customer reviews of specialty hot sauces. Use Node.js, Express, and MongoDB to build its NoSQL database. Secure customer data by applying the OWASP web security standards.",
             tech: "Node js, Express, bcrypt, multer, MongoDb, GitHub",
             imageURL: ["so_peckoko_log.PNG", "so_peckoko_create.PNG", "so_peckoko_CRUD.PNG"]
         },
         {
             id: '6',
             name: "Groupomania",
-            summary: "Création d'un réseau social interne à une entreprise",
-            description: "Personnaliser le contenu envoyé à un client web, Gérer un stockage de données à l'aide de SQL, Implémenter un stockage de données sécurisé en utilisant SQL, Authentifier un utilisateur et maintenir sa session, Définir des rôles et des privilèges suivant l'utilisateur",
+            summary: "Build a Full-Stack Solution",
+            description: "Analyze a client’s needs to define the scope and features of a new application. With the help of a front-end framework, build a full-stack solution, including a SQL database.",
             tech: "Mysql, Node js, Express, bcrypt, multer, bootstrap, Vue js, GitHub",
             imageURL: ["groupomania_log.png", "groupomania_signup.png", "groupomania_home.png", "groupomania_delete.png", "groupomania_forum.png", "groupomania_update.png", "groupomania_empty.png"]
         }
--- pages/projets.vue
+++ pages/projets.vue
@@ -4,7 +4,7 @@
         <p class="reveal-1">Cliquer sur un projet pour en savoir plus: technologies utilisées, objectifs attendus...</p>
         <div >
             <transition-group class="page__projet--layout" name="slide-fade" tag="article" appear>
-                <NuxtLink v-for="item,l in project" :key="item.id" :style="{'--i': l}" class="page__projet--link btn--projets" :to="localePath({ name: 'projet-id', params: {id: item.id}})">
+                <NuxtLink v-for="item,l in $t('projects')" :key="item.id" :style="{'--i': l}" class="page__projet--link btn--projets" :to="localePath({ name: 'projet-id', params: {id: item.id}})">
                     <article>
                         <img src="" alt="" class="page__projet--image">
                         <h2>{{uppercase(item.name)}}</h2>
@@ -19,20 +19,14 @@
 </template>
 
 <script>
-import projects from '@/store/projects.js'
 import { uppercase, reveal } from '@/store/function.js'
 
 export default {
     data() {
         return{
-            project: projects.projects
         }
     },
-    computed: {
-  availableLocales () {
-    return this.$i18n
-  }
-},
+    
     methods: {
         uppercase
     },
