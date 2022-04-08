ce807444589bdfb2016b45811ecb574e62263960
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-04-08 16:30:27 +0700

    maj dist

--- dist/_nuxt/static/1645180713/projet/5/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645180713",layout:"default",error:a,state:{about:{},function:{},projects:{},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fprojet\u002F5",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1649409764/en/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649409764",layout:"default",error:a,state:{about:{},function:{},projects:{},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fen\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
