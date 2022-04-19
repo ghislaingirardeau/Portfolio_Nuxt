bd100bebfefc6a84fe87762b20ef26196ca40f69
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-04-19 17:01:22 +0700

    maj dist

--- dist/_nuxt/static/1649409764/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649409764",layout:"default",error:a,state:{about:{},function:{},projects:{},i18n:{routeParams:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
