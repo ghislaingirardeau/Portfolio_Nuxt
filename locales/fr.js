ecf3fdc765f344ece19f50f2093fdf21ee18cb33
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-04-19 17:31:07 +0700

    remove img bug ohmyfood

--- locales/fr.js
+++ locales/fr.js
@@ -121,7 +121,7 @@ export default{
             summary: "Mise en œuvre une structure de navigation web",
             description: "Mettre en œuvre des effets CSS graphiques avancés en assurant une cohérence graphique. Mise en place d'animation Keyframes et transitions pour rendre enrichir l'expérience utilisateur",
             tech: "HTML, CSS, SASS, GitHub, méthode BEM pour un code clair",
-            imageURL: ["ohmyfood.PNG", "ohmyfood_menu.PNG", "ohmyfood_menu1.PNG"],
+            imageURL: ["ohmyfood.PNG", "ohmyfood_menu1.PNG"],
             loader: 'true',
             link: 'https://ghislaingirardeau.github.io/GirardeauGhislain_3_13012021/index.html'
         },
